9129e9e8f43404a6080c015a9420454636141730
sharad choudhary <126180911+sharad102003@users.noreply.github.com>
2024-08-14 22:06:44 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser()); 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://classroom-management-frontend.vercel.app',
   credentials: true 
 })); 
 
